340da02b6f7e824c1a7b2a18281a6c89ca99f21f
BrodoTbaggins <austin.sixpack@gmail.com>
2018-01-05 21:38:11 -0700

    Got distance and duration to show

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import GetDistance from './Components/GetDistance/GetDistance';
+import GetTrip from './Components/GetTrip/GetTrip';
 
 
 class App extends Component {
@@ -19,7 +19,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <GetDistance />
+        <GetTrip />
       </div>
     );
   }
--- src/Components/GetTrip/GetTrip.js
+++ src/Components/GetTrip/GetTrip.js
@@ -1,26 +1,24 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 
-export default class GetDistance extends Component{
+export default class getTrip extends Component{
     constructor(){
         super();
         this.state = {
-            tripsToDisplay: [],
             origin: '',
             destination: '',
             data: null
         }
-    this.getDistance = this.getDistance.bind(this);    
+    this.getTripData = this.getTripData.bind(this);    
     this.handleUpdateOrigin = this.handleUpdateOrigin.bind(this);
     this.handleUpdateDestination = this.handleUpdateDestination.bind(this);
     }
 
-    getDistance(){
+    getTripData(){
         axios.get(`/api/trips/${this.state.origin}/${this.state.destination}`)
-        .then(response => this.setState({data: response.data}))
-
-        
+        .then(response => this.setState({data: response.data}))  
     }
+
 
     handleUpdateOrigin(e){
         this.setState({origin: e.target.value})
@@ -36,8 +34,21 @@ export default class GetDistance extends Component{
                  <div className="form">
                     <input type="text" placeholder="Origin" onChange={this.handleUpdateOrigin}/>
                     <input type="text" placeholder="Destination" onChange={this.handleUpdateDestination}/>
-                    <button onClick={this.getDistance}>Get Distance</button>
+                    <button onClick={this.getTripData}>Get Distance</button>
                 </div>
+
+                <div>
+                   {this.state.data &&
+                    <div>
+                        <h1>Origin: {this.state.data.origin_addresses}</h1>
+                        <h1>Destination: {this.state.data.destination_addresses}</h1>
+                        <h3>Distance: {this.state.data.rows[0].elements[0].distance.text}</h3>
+                        <h3>Duration: {this.state.data.rows[0].elements[0].duration.text}</h3>
+                        </div>
+                    }
+                    
+                </div>
+
             </div>
         )
     }
